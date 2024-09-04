67f8aa9b242506866cf1cea109b118678cbae2a2
yotech1324 <105988202+yotech1324@users.noreply.github.com>
2024-09-05 00:18:41 +0530

    Update payment.js

--- models/payment.js
+++ models/payment.js
@@ -5,7 +5,8 @@ const PaymentSchema = mongoose.Schema({
     razorpay_signature: { type: String, required: true },
     razorpay_payment_id: { type: String, required: true },
     User_name:{type:String , required: true},
-    userId:{type:String, required: true}
+    userId:{type:String, required: true},
+    orderAmount:{type:String }
 })
 
 export const Payment = mongoose.model("Payment", PaymentSchema )
